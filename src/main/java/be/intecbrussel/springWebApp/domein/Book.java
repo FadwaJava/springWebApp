1f244a2f9b7e865348818277729bc3a5d134d322
FadwaJava <fadwa.elidrissi@contactoffice.be>
2021-01-20 11:45:04 +0100

    books list ok

--- src/main/java/be/intecbrussel/springWebApp/domein/Book.java
+++ src/main/java/be/intecbrussel/springWebApp/domein/Book.java
@@ -27,6 +27,14 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
